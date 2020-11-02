92a0b9b7af7233dbb85d8eb9c606ff873d8989e4
Celine_lhp5 <celine.louvel.76@outlook.fr>
2020-11-02 10:14:53 +0100

    ajout ok

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+let firstNumber = prompt('nombre 1');
+let secondNumber = prompt('nombre 2');
+let resultat = parseInt(firstNumber) + parseInt(secondNumber);
+    alert(resultat);
+    console.log("firstNumber + secondNumber = resultat");
