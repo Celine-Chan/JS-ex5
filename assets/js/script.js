7b6460a2bbc823f1e87a748b789bd03ce134d1a3
Celine_lhp5 <celine.louvel.76@outlook.fr>
2020-11-02 12:05:02 +0100

    modif ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,5 @@ let secondNumber = prompt('nombre 2');
 let resultat = parseInt(firstNumber) + parseInt(secondNumber); 
 // let resultat = +firstNumber + +secondNumber; = remplace le parseInt
 
-    alert(firstNumber + ' + ' + secondNumber + ' = ' + resultat);
-    console.log("firstNumber + secondNumber = resultat");
+alert(firstNumber + ' + ' + secondNumber + ' = ' + resultat);
+console.log(firstNumber + ' + ' + secondNumber + ' = ' + resultat);
